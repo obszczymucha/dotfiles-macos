8f1ab2db4036d6b6646c11f758dcea673c6ae629
Obszczymucha <obszczymucha@gmail.com>
2025-08-26 21:06:37 +1000

    Adjust tmux_smart_split (also remove buggy @last-window-index).

--- Scripts/tmux_smart_split.sh
+++ Scripts/tmux_smart_split.sh
@@ -8,6 +8,10 @@ count_panes() {
   tmux list-panes | wc -l
 }
 
+random_name() {
+  echo "win_$((RANDOM % 100))"
+}
+
 window_exists() {
   local window_list
   window_list=$(tmux list-windows -F '#{window_name}')
@@ -30,13 +34,32 @@ break_pane() {
   current_window_index=$(tmux display-message -p '#{window_index}')
 
   if [[ -n "$window_name" && "$window_name" != "" ]]; then
-    local last_window_index
-    last_window_index=$(tmux show-option -q -p -t "${current_window_index}.${pane_count}" -v @last-window-index)
-    last_window_index=$((last_window_index - 1))
+    # local last_window_index
+    # last_window_index=$(tmux show-option -q -p -t "${current_window_index}.${pane_count}" -v @last-window-index)
+    # last_window_index=$((last_window_index - 1))
 
     tmux break-pane -s "$pane_count" -d -a -n "$window_name" ${last_window_index:+-t ":$last_window_index"}
   else
-    tmux break-pane -s "$pane_count" -d -a
+    local new_name
+    new_name=$(random_name)
+    tmux break-pane -s "$pane_count" -d -a -n "$new_name"
+  fi
+
+  local current_window_pane_count
+  current_window_pane_count=$(tmux display-message -p '#{window_panes}')
+
+  if [[ "$current_window_pane_count" -eq 1 ]]; then
+    local window_name
+    window_name=$(tmux show-option -p -t 1 -qv @window-name)
+
+    if [[ -n "$window_name" && "$window_name" != "" ]]; then
+      tmux rename-window -t "${current_window_index}" "$window_name"
+    else
+      local new_name
+      new_name=$(random_name)
+      tmux rename-window -t "${current_window_index}" "$new_name"
+      tmux set-option -p -t ".1" @window-name "$new_name"
+    fi
   fi
 }
 
@@ -71,6 +94,7 @@ join_pane() {
   local new_split_size="$2"
   local swap_window_name="$3"
   local orientation="$4"
+  local params="$5"
 
   local current_window_index
   current_window_index=$(tmux display-message -p '#{window_index}')
@@ -91,12 +115,12 @@ join_pane() {
   local current_window_index
   swap_window_index=$(tmux display-message -t "${swap_window_name}" -p '#{window_index}')
 
-  tmux set -p -t "${swap_window_name}.1" @last-window-index "$swap_window_index"
+  # tmux set -p -t "${swap_window_name}.1" @last-window-index "$swap_window_index"
 
   if [[ "$orientation" == "h" ]]; then
-    tmux join-pane -d -s "${swap_window_name}.1" -t "${current_window_index}.${pane_count}" -h ${new_split_size:+-l "$new_split_size"}
+    tmux join-pane ${params:+"$params"} -s "${swap_window_name}.1" -t "${current_window_index}.${pane_count}" -h ${new_split_size:+-l "$new_split_size"}
   else
-    tmux join-pane -d -s "${swap_window_name}.1" -t "${current_window_index}.${pane_count}" -v ${new_split_size:+-l "$new_split_size"}
+    tmux join-pane ${params:+"$params"} -s "${swap_window_name}.1" -t "${current_window_index}.${pane_count}" -v ${new_split_size:+-l "$new_split_size"}
   fi
 
   if tmux list-windows -f "#{==:#W,${swap_window_name}}" -F '#W' | grep -q .; then
@@ -106,7 +130,9 @@ join_pane() {
     if [[ -n "$new_window_name" && "$new_window_name" != "" ]]; then
       tmux rename-window -t "${swap_window_name}" "$new_window_name"
     else
-      tmux rename-window -t "${swap_window_name}" "window_$((RANDOM % 100))"
+      local new_name
+      new_name=$(random_name)
+      tmux rename-window -t "${swap_window_name}" "$new_name"
     fi
   fi
 
@@ -163,6 +189,7 @@ main() {
   local orientation="$1" # h or v
   local new_split_size="$2" # horizontal or vertical (will split in half if not provided)
   local swap_window_name="$3"
+  local params="$4"
 
   local window_count
   window_count=$(count_windows)
@@ -204,7 +231,7 @@ main() {
     return
   fi
 
-  join_pane "$pane_count" "$new_split_size" "$swap_window_name" "$orientation"
+  join_pane "$pane_count" "$new_split_size" "$swap_window_name" "$orientation" "$params"
   return
 }
 
